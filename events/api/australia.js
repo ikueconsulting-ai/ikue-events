1cee0f3e05360c830b51c91024a6d878168a5f26
ikueconsulting-ai <ikueconsulting@gmail.com>
2026-06-16 15:15:42 +0700

    Update australia.js

--- events/api/australia.js
+++ events/api/australia.js
@@ -1,1 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "events", "australia.json");
+  const json = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  res.status(200).json(json);
+}
 
