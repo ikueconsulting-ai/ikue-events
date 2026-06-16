eef59ccea99d313799855e030acd1667de25978e
ikueconsulting-ai <ikueconsulting@gmail.com>
2026-06-16 13:42:43 +0700

    Delete api directory

--- api/africa.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "events", "africa.json");
-  const json = JSON.parse(fs.readFileSync(filePath, "utf8"));
-  res.status(200).json(json);
-}
